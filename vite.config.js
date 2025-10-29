a9323c1acd1422f88eae62844c5612113f8ea3b8
harsh637836 <harshshrimali213@gmail.com>
2025-10-29 09:53:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
-```
-
-- Click **"Commit changes"**
-
----
-
-### **File 4: .gitignore**
-
-- Create new file: `.gitignore`
-- Copy this:
-```
-node_modules
-dist
-.DS_Store
+});
